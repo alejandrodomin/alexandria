ee08de310017ecbdec868247c06890bf47caebc6
alejandrodomin <alexdominguez169@gmail.com>
2025-04-29 22:30:52 -0400

    General logic for transpiler.py
    Committed from iPhone

--- transpiler.py
+++ transpiler.py
@@ -1,6 +1,12 @@
 with open("filename", "r") as file:
     for line in file:
+        
+        transpiled_line=[]
         for word in line.split(" "):
             if word in word_map:
+                transpiled_line.append(word_map[word])
+            else:
+                transpiled_line.append(word)
                 
+        print(" ".join(transpiled_line))
         
