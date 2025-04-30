f588456e9d211960054a5f0958d7cd49aff9306e
A283le <alexdominguez169@gmail.com>
2025-04-30 00:12:51 -0400

    Adding some more test cases to the test file. This is definetly super dumb, I'm grinning ear to ear though.

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-count = 0
-for num in range ( 100 ) : 
-	count += num
+def count ( to ) : 
+	count = 0
+	for num in range ( to ) : 
+		count += num
+	
+	return count 
 
-print ( count ) 
+print ( count ( 100 ) ) 
+
+
+def my_min ( num_arr ) : 
+	return min ( num_arr ) 
+
+print ( my_min ( [ 10 , 12 , 2 , ] ) ) 
