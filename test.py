b7505e883d84fddbf1c94bcabc30a7d664158da0
A283le <alexdominguez169@gmail.com>
2025-04-29 23:54:52 -0400

    Encountered my first bug, python syntax does not like this many spaces.

---
+++ test.py
@@ -0,0 +1,5 @@
+count = 0
+for num in range ( 100 ) : 
+	count + = num
+
+print ( count ) 
--- transpiler.py
+++ transpiler.py
@@ -1,24 +1,43 @@
-word_map = { "plus": "+", "minus": "-", "mltp": "*", "div": "/",  
+import argparse
+
+def transpile(file):
+    word_map = { "plus": "+", "minus": "-", "mltp": "*", "div": "/",  
             "mod": "%", "fdiv": "//", "iseq": "==", "noteq": "!=", 
             "gt": ">", "lt": "<", "gteq": ">=", "lteq": "<=",
             "par": "(", "epar": ")", "sqr": "[", "esqr": "]",
             "dct": "{", "edct": "}", "com": "," ,"then": ":",
-            "hash": "#",}
- 
-transpiled_lines = []
-with open("test.ipy", "r") as file:
-    for line in file:
-        transpiled_line=[]
-        for word in line.split(" "):
-            if word in word_map:
-                transpiled_line.append(word_map[word])
-            else:
-                transpiled_line.append(word)
+            "hash": "#", "eq": "="}
+    new_name = file.replace("ipy", "py")
+    transpiled_lines = []
+
+    
+    with open(file, "r") as file:
+        for line in file:
+            transpiled_line=[]
+            for word in line.split(" "):
+                if word in word_map:
+                    transpiled_line.append(word_map[word])
+                else:
+                    transpiled_line.append(word)
                 
-        transpiled_lines.append(" ".join(transpiled_line))
+            transpiled_lines.append(" ".join(transpiled_line))
 
-with open("test.py", "w") as file:
-    for line in transpiled_lines:
-        file.write(line)
+    with open(new_name, "w") as file:
+        for line in transpiled_lines:
+            file.write(line)
 
-      
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description="Transpiler for the Ithon language. Ithon is meant as a no special character language for use within mobile environments or where ever the keyboard is limited.")
+
+    # Add arguments
+    parser.add_argument("--file", help="Ithon file ending in .ipy to convert to python", 
+                        required=True, type=str)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Access arguments
+    file=args.file
+    print(file)
+
+    transpile(file)
